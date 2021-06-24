0bbf4099c1b3a4f105244054891096149c9ed61d
Arya Soni <aryasoni98@gmail.com>
2021-06-24 17:42:39 +0530

    Update

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,228 @@
+(function() {
+var exports = {};
+exports.id = 660;
+exports.ids = [660];
+exports.modules = {
+
+/***/ 53:
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": function() { return /* binding */ Document; }
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(282);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(297);
+// EXTERNAL MODULE: ./node_modules/next/document.js
+var next_document = __webpack_require__(859);
+// EXTERNAL MODULE: external "@stitches/react"
+var react_ = __webpack_require__(459);
+;// CONCATENATED MODULE: ./stitches.config.ts
+
+const {
+  css,
+  styled,
+  global,
+  getCssString
+} = (0,react_.createCss)({
+  theme: {
+    colors: {
+      hiContrast: 'hsl(206,10%,5%)',
+      loContrast: 'white',
+      gray100: 'hsl(206,22%,99%)',
+      gray200: 'hsl(206,12%,97%)',
+      gray300: 'hsl(206,11%,92%)',
+      gray400: 'hsl(206,10%,84%)',
+      gray500: 'hsl(206,10%,76%)',
+      gray600: 'hsl(206,10%,44%)',
+      purple100: 'hsl(252,100%,99%)',
+      purple200: 'hsl(252,100%,98%)',
+      purple300: 'hsl(252,100%,94%)',
+      purple400: 'hsl(252,75%,84%)',
+      purple500: 'hsl(252,78%,60%)',
+      purple600: 'hsl(252,80%,53%)'
+    },
+    space: {
+      1: '5px',
+      2: '10px',
+      3: '15px',
+      4: '20px',
+      5: '25px',
+      6: '35px'
+    },
+    sizes: {
+      1: '5px',
+      2: '10px',
+      3: '15px',
+      4: '20px',
+      5: '25px',
+      6: '35px'
+    },
+    fontSizes: {
+      1: '12px',
+      2: '13px',
+      3: '15px',
+      4: '17px',
+      5: '19px',
+      6: '21px'
+    },
+    fonts: {
+      system: 'system-ui'
+    }
+  },
+  utils: {
+    marginX: config => value => ({
+      marginLeft: value,
+      marginRight: value
+    }),
+    marginY: config => value => ({
+      marginTop: value,
+      marginBottom: value
+    }),
+    paddingX: config => value => ({
+      paddingLeft: value,
+      paddingRight: value
+    }),
+    paddingY: config => value => ({
+      paddingTop: value,
+      paddingBottom: value
+    })
+  },
+  media: {
+    bp1: '@media (min-width: 520px)',
+    bp2: '@media (min-width: 900px)'
+  }
+});
+;// CONCATENATED MODULE: ./pages/_document.tsx
+
+
+
+
+
+class Document extends next_document.default {
+  render() {
+    return /*#__PURE__*/(0,jsx_runtime_.jsxs)(next_document.Html, {
+      lang: "en",
+      children: [/*#__PURE__*/jsx_runtime_.jsx(next_document.Head, {
+        children: /*#__PURE__*/jsx_runtime_.jsx("style", {
+          id: "stitches",
+          dangerouslySetInnerHTML: {
+            __html: getCssString()
+          }
+        })
+      }), /*#__PURE__*/(0,jsx_runtime_.jsxs)("body", {
+        children: [/*#__PURE__*/jsx_runtime_.jsx(next_document.Main, {}), /*#__PURE__*/jsx_runtime_.jsx(next_document.NextScript, {})]
+      })]
+    });
+  }
+
+}
+
+/***/ }),
+
+/***/ 459:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("@stitches/react");;
+
+/***/ }),
+
+/***/ 227:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/constants.js");;
+
+/***/ }),
+
+/***/ 932:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/document-context.js");;
+
+/***/ }),
+
+/***/ 816:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/head-manager-context.js");;
+
+/***/ }),
+
+/***/ 579:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/lib/utils.js");;
+
+/***/ }),
+
+/***/ 171:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/server/get-page-files.js");;
+
+/***/ }),
+
+/***/ 105:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("next/dist/next-server/server/utils.js");;
+
+/***/ }),
+
+/***/ 229:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("prop-types");;
+
+/***/ }),
+
+/***/ 297:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react");;
+
+/***/ }),
+
+/***/ 282:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("react/jsx-runtime");;
+
+/***/ }),
+
+/***/ 168:
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("styled-jsx/server");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = __webpack_require__.X(0, [859], function() { return __webpack_exec__(53); });
+module.exports = __webpack_exports__;
+
+})();
